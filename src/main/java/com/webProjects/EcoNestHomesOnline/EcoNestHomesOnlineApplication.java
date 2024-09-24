48c12b9e039285eb9bbc460a99c739087753653b
Yoan Kalafirov <joankalafirov@gmail.com>
2024-09-24 23:06:24 +0300

    git init

---
+++ src/main/java/com/webProjects/EcoNestHomesOnline/EcoNestHomesOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.webProjects.EcoNestHomesOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoNestHomesOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoNestHomesOnlineApplication.class, args);
+	}
+
+}
